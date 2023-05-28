c11782b79342f2cf1632fce3d0061e2b90e2af66
Ryan Corrigan <ryandcorrigan@gmail.com>
2023-05-27 18:42:58 -0600

    removed a console log

--- src/App.js
+++ src/App.js
@@ -87,7 +87,6 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    console.log(clarifaiFace);
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
